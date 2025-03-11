4751b23413c1835d219461481114d8dd5c45151e
Calnce <102400947+Calnce@users.noreply.github.com>
2025-03-12 00:45:28 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-// client.c (Raspberry Pi)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,152 +9,187 @@
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
+#include <dirent.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <openssl/sha.h>
+#include <time.h>
 
 #define PORT 8443
 #define BUFFER_SIZE 1024
 #define I2C_DEVICE "/dev/i2c-1"
-#define ESP32_ADDR 0x08  // I2C address of ESP32
+#define ESP32_ADDR 0x08
+#define SHA256_DIGEST_LENGTH 32
 
-void init_openssl() {
-    SSL_library_init();
-    SSL_load_error_strings();
-    OpenSSL_add_all_algorithms();
-}
+// Opcode extraction functions
+void read_executable_segments(pid_t pid, FILE *output) {
+    char maps_path[256], mem_path[256];
+    snprintf(maps_path, sizeof(maps_path), "/proc/%d/maps", pid);
+    snprintf(mem_path, sizeof(mem_path), "/proc/%d/mem", pid);
 
-void cleanup_openssl() {
-    EVP_cleanup();
-}
-
-SSL_CTX *create_context() {
-    const SSL_METHOD *method;
-    SSL_CTX *ctx;
-
-    method = TLS_client_method();
-    ctx = SSL_CTX_new(method);
-    if (!ctx) {
-        perror("Unable to create SSL context");
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+    FILE *maps_file = fopen(maps_path, "r");
+    if (!maps_file) {
+        perror("Failed to open maps file");
+        return;
     }
 
-    return ctx;
+    int mem_fd = open(mem_path, O_RDONLY);
+    if (mem_fd == -1) {
+        perror("Failed to open mem file");
+        fclose(maps_file);
+        return;
+    }
+
+    char line[512];
+    while (fgets(line, sizeof(line), maps_file)) {
+        uintptr_t start, end;
+        char perms[5];
+        if (sscanf(line, "%lx-%lx %4s", &start, &end, perms) != 3) continue;
+        if (perms[2] != 'x') continue;
+
+        size_t size = end - start;
+        unsigned char *buffer = malloc(size);
+        if (!buffer) {
+            perror("Failed to allocate buffer");
+            continue;
+        }
+
+        if (lseek(mem_fd, start, SEEK_SET) == (off_t)-1) {
+            perror("Failed to seek");
+            free(buffer);
+            continue;
+        }
+
+        ssize_t bytes_read = read(mem_fd, buffer, size);
+        if (bytes_read > 0) {
+            fprintf(output, "Opcodes from %lx to %lx (%zd bytes):\n", start, end, bytes_read);
+            for (ssize_t i = 0; i < bytes_read; i++) {
+                fprintf(output, "%02x ", buffer[i]);
+                if ((i + 1) % 16 == 0) fprintf(output, "\n");
+            }
+            fprintf(output, "\n\n");
+        }
+        free(buffer);
+    }
+
+    fclose(maps_file);
+    close(mem_fd);
 }
 
-void configure_context(SSL_CTX *ctx) {
-    if (SSL_CTX_use_certificate_file(ctx, "../certs/client.crt", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+// Hashing functions
+int calculate_file_hash(const char *filepath, unsigned char *hash_output) {
+    FILE *file = fopen(filepath, "rb");
+    if (!file) return -1;
+
+    SHA256_CTX sha256_ctx;
+    SHA256_Init(&sha256_ctx);
+
+    unsigned char buffer[4096];
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        SHA256_Update(&sha256_ctx, buffer, bytes_read);
     }
 
-    if (SSL_CTX_use_PrivateKey_file(ctx, "../certs/client.key", SSL_FILETYPE_PEM) <= 0) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    if (!SSL_CTX_load_verify_locations(ctx, "../certs/ca.crt", NULL)) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+    SHA256_Final(hash_output, &sha256_ctx);
+    fclose(file);
+    return 0;
 }
 
+// I2C functions
 int setup_i2c() {
     int i2c_fd = open(I2C_DEVICE, O_RDWR);
-    if (i2c_fd < 0) {
-        perror("Failed to open I2C bus");
-        return -1;
-    }
-
-    if (ioctl(i2c_fd, I2C_SLAVE, ESP32_ADDR) < 0) {
-        perror("Failed to set I2C slave address");
-        return -1;
-    }
-
+    if (i2c_fd < 0) return -1;
+    if (ioctl(i2c_fd, I2C_SLAVE, ESP32_ADDR) < 0) return -1;
     return i2c_fd;
 }
 
 void send_to_i2c(int i2c_fd, const char *message) {
-    if (write(i2c_fd, message, strlen(message)) != strlen(message)) {
-        perror("Failed to write to I2C");
-    }
+    write(i2c_fd, message, strlen(message));
 }
 
-void read_from_i2c(int i2c_fd, char *buffer, int length) {
-    if (read(i2c_fd, buffer, length) != length) {
-        perror("Failed to read from I2C");
-    }
+// SSL functions
+SSL_CTX *create_context() {
+    const SSL_METHOD *method = TLS_client_method();
+    SSL_CTX *ctx = SSL_CTX_new(method);
+    if (!ctx) exit(EXIT_FAILURE);
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx) {
+    SSL_CTX_use_certificate_file(ctx, "../certs/client.crt", SSL_FILETYPE_PEM);
+    SSL_CTX_use_PrivateKey_file(ctx, "../certs/client.key", SSL_FILETYPE_PEM);
+    SSL_CTX_load_verify_locations(ctx, "../certs/ca.crt", NULL);
 }
 
 int main() {
-    const char *hostname = "192.168.59.41";  // Server IP
+    const char *hostname = "192.168.59.41";
     struct sockaddr_in addr;
-    SSL_CTX *ctx;
-    SSL *ssl;
-    int sock;
-
-    init_openssl();
-    ctx = create_context();
+    SSL_CTX *ctx = create_context();
     configure_context(ctx);
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("Unable to create socket");
-        exit(EXIT_FAILURE);
-    }
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
 
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = inet_addr(hostname);
 
-    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("Unable to connect");
-        exit(EXIT_FAILURE);
-    }
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
 
-    ssl = SSL_new(ctx);
+    SSL *ssl = SSL_new(ctx);
     SSL_set_fd(ssl, sock);
+    SSL_connect(ssl);
 
-    if (SSL_connect(ssl) <= 0) {
-        ERR_print_errors_fp(stderr);
-    } else {
+    // Initial hash generation
+    FILE *opcode_file = fopen("extractedOpcodes.txt", "w");
+    read_executable_segments(getpid(), opcode_file);
+    fclose(opcode_file);
+
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    calculate_file_hash("extractedOpcodes.txt", hash);
+
+    char hash_msg[2 * SHA256_DIGEST_LENGTH + 6];
+    snprintf(hash_msg, sizeof(hash_msg), "HASH:");
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        snprintf(hash_msg + 5 + i*2, 3, "%02x", hash[i]);
+    }
+    SSL_write(ssl, hash_msg, strlen(hash_msg));
+
+    // Main loop
+    int i2c_fd = setup_i2c();
+    time_t last_check = time(NULL);
+    while (1) {
+        // Periodic hash check
+        if (time(NULL) - last_check >= 120) {
+            FILE *opcode_file = fopen("extractedOpcodes.txt", "w");
+            read_executable_segments(getpid(), opcode_file);
+            fclose(opcode_file);
+            
+            unsigned char new_hash[SHA256_DIGEST_LENGTH];
+            calculate_file_hash("extractedOpcodes.txt", new_hash);
+            
+            char new_hash_msg[2 * SHA256_DIGEST_LENGTH + 6];
+            snprintf(new_hash_msg, sizeof(new_hash_msg), "HASH:");
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+                snprintf(new_hash_msg + 5 + i*2, 3, "%02x", new_hash[i]);
+            }
+            SSL_write(ssl, new_hash_msg, strlen(new_hash_msg));
+            last_check = time(NULL);
+        }
+
+        // Existing ticket logic
         char buffer[BUFFER_SIZE];
-        int i2c_fd = setup_i2c();
-        if (i2c_fd == -1) exit(EXIT_FAILURE);
-
-        // Receive handshake from server
+        SSL_write(ssl, "TICKET\n", 7);
         int bytes = SSL_read(ssl, buffer, BUFFER_SIZE - 1);
         if (bytes > 0) {
             buffer[bytes] = '\0';
-            send_to_i2c(i2c_fd, buffer);  // Send handshake to ESP32
+            send_to_i2c(i2c_fd, buffer);
         }
-
-        // Send tickets every 5 seconds
-        int count = 0;
-        while (1) {
-            sleep(1);
-            if(count<10){
-              SSL_write(ssl, "TICKET\n", 7);
-            }
-            else{
-              SSL_write(ssl, "HACKED\n", 7);
-            }
-            count = count + 1;
-            bytes = SSL_read(ssl, buffer, BUFFER_SIZE - 1);
-            if (bytes > 0) {
-                buffer[bytes] = '\0';
-                send_to_i2c(i2c_fd, buffer);  // Send ticket to ESP32
-            }
-        }
-
-        close(i2c_fd);
+        sleep(1);
     }
 
+    close(i2c_fd);
     SSL_shutdown(ssl);
     SSL_free(ssl);
     close(sock);
     SSL_CTX_free(ctx);
-    cleanup_openssl();
-
     return 0;
 }
